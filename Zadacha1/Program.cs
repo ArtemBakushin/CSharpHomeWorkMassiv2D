17ac92a6f62dd58e9b6bf06673f33b662c67471b
artem <artem28282828@gmail.com>
2023-06-26 15:17:39 +0500

    Задача 1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+Console.Write("Введите количевство строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количевство столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = GetMatrix( row , column , 1 , 10);
+PrintMatrix(matrix);
+DecreasingElement(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
+
+int [,] GetMatrix (int m, int n , int min , int max )
+{
+    int [,] array = new int [m,n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(min,max);
+        }
+        
+    }
+    return array;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void DecreasingElement(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
